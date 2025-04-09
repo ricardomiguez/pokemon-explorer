ff45819cca51621e6c55951c79f222e44bd87465
ricardomiguez <ricardo.miguez@icloud.com>
2025-04-09 17:07:50 +0200

    build(heroui): install @heroui/react and update Tailwind CSS configuration

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,3 +1,4 @@
+import { HeroUIProvider } from "@heroui/system";
 import "./globals.css";
 
 export default function RootLayout({
@@ -7,7 +8,9 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body>
+        <HeroUIProvider>{children}</HeroUIProvider>
+      </body>
     </html>
   );
 }
