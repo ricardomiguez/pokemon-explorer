f38c34b9777d7b92ff7af28ec67bc00ab0df4bec
ricardomiguez <ricardo.miguez@icloud.com>
2025-04-09 17:48:09 +0200

    feat(layout): add font configuration and refactor root layout structure

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,5 +1,7 @@
 import { HeroUIProvider } from "@heroui/system";
 import "./globals.css";
+import { fontSans } from "@/config/fonts";
+import { cn } from "@heroui/react";
 
 export default function RootLayout({
   children,
@@ -8,8 +10,19 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body>
-        <HeroUIProvider>{children}</HeroUIProvider>
+      <body
+        className={cn(
+          "min-h-screen bg-background font-sans antialiased",
+          fontSans.className
+        )}
+      >
+        <HeroUIProvider>
+          <div className="relative flex flex-col h-screen">
+            <main className="container mx-auto max-w-7xl pt-16 px-6 flex-grow">
+              {children}
+            </main>
+          </div>
+        </HeroUIProvider>
       </body>
     </html>
   );
