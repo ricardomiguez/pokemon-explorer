db95259cdce25ffdb4b1c9c2d6cd707d6217045f
ricardomiguez <ricardo.miguez@icloud.com>
2025-04-10 18:29:47 +0200

    feat(navbar): add responsive layout with navbar menu

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,23 +1,33 @@
+"use client";
+
 import {
   Navbar as HeroUINavbar,
   NavbarContent,
   NavbarBrand,
   NavbarItem,
+  NavbarMenu,
+  NavbarMenuItem,
+  NavbarMenuToggle,
 } from "@heroui/navbar";
 import { link as linkStyles } from "@heroui/theme";
 import NextLink from "next/link";
-import { cn } from "@heroui/react";
+import { cn, Link } from "@heroui/react";
 
 import { websiteConfig } from "@/config/website";
 import { PokeballIcon } from "./icons/pokeball-icon";
 import { messages } from "@/messages/en";
+import { useState } from "react";
 
 export const Navbar = () => {
+  const [isMenuOpen, setIsMenuOpen] = useState(false);
+
   return (
     <HeroUINavbar
       className="shadow-black/5 shadow-small"
       maxWidth="xl"
       position="sticky"
+      isMenuOpen={isMenuOpen}
+      onMenuOpenChange={setIsMenuOpen}
     >
       <NavbarContent className="basis-1/5 sm:basis-full" justify="start">
         <NavbarBrand as="li" className="gap-3 max-w-fit">
@@ -26,7 +36,7 @@ export const Navbar = () => {
             <p className="font-bold text-inherit">{messages.navBar.title}</p>
           </NextLink>
         </NavbarBrand>
-        <ul className="hidden lg:flex gap-4 justify-start ml-2">
+        <ul className="hidden sm:flex gap-4 justify-start ml-2">
           {websiteConfig.navItems.map((item) => (
             <NavbarItem key={`navbar_${item.label}_item`}>
               <NextLink
@@ -43,6 +53,24 @@ export const Navbar = () => {
           ))}
         </ul>
       </NavbarContent>
+
+      <NavbarContent className="sm:hidden" justify="end">
+        <NavbarMenuToggle
+          aria-label={isMenuOpen ? "Close menu" : "Open menu"}
+        />
+      </NavbarContent>
+
+      <NavbarMenu>
+        <div className="mx-4 mt-2 flex flex-col gap-2">
+          {websiteConfig.navItems.map((item) => (
+            <NavbarMenuItem key={`navbar_menu_${item.label}_item`}>
+              <Link color="foreground" href={item.href} size="lg">
+                {item.label}
+              </Link>
+            </NavbarMenuItem>
+          ))}
+        </div>
+      </NavbarMenu>
     </HeroUINavbar>
   );
 };
